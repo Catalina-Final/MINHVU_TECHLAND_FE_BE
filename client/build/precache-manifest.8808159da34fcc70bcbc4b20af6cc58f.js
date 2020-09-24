917013821bcc393b21bbd7e2d2783833ec0bab66
Minh Vu <minhvu72826@gmail.com>
2020-09-24 19:19:22 +0700

    build react project

---
+++ client/build/precache-manifest.8808159da34fcc70bcbc4b20af6cc58f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1fdad360866fafd82dc1e5c48f5bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ddcb7c8d2d1b3d2d496",
+    "url": "/static/css/main.418ec78f.chunk.css"
+  },
+  {
+    "revision": "2a4ea107195c1dfca52b",
+    "url": "/static/js/2.307afe9f.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.307afe9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddcb7c8d2d1b3d2d496",
+    "url": "/static/js/main.f993d00d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a9120fb5529cd9fb696bc757903be467",
+    "url": "/static/media/pic0.a9120fb5.jpg"
+  }
+]);
