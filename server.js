1b43100b2518d4e7e4e3426e23f3adc9efa5308c
Minh Vu <minhvu72826@gmail.com>
2020-09-24 17:57:39 +0700

    modify server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express"),
+const path = require("path"),
+  express = require("express"),
   app = express(),
   port = process.env.PORT || 5000,
   connectDB = require("./config/db"),
@@ -18,5 +19,15 @@ app.use("/api/users", usersRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/profile", profileRoutes);
+
+// serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`server's listening on port ${port}`));
